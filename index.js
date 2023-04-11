119cd198ec6a4e0d7d07f761fa5dad1d8fdff416
Christopher Lozzi <clozzi12@gmail.com>
2023-04-11 14:57:22 -0400

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(name, surprise) {
+  let thankYous = [];
+  for (let i = 0; i < name.length; i++) {
+    thankYous.push(`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`);
+  }
+  return thankYous;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
